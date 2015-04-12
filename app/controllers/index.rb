598ab2a0c14d52ca1825b7367ea7e657a573e901
Lynne Tye <lynnetye@gmail.com>
2015-04-12 05:23:27 -0700

    complete without ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,8 +1,41 @@
 get '/' do
-  # Look in app/views/index.erb
+  @all_notes = Note.all
+  # (:order => [ :id.desc ], :limit => 20)
+  @only_display = true
+
   erb :index
 end
 
-get '/info' do
-  Demo.new(self).info
+post '/new' do
+  @note = Note.create!(content: params[:content])
+
+  redirect '/'
 end
+
+get '/notes/:index/edit' do
+  @all_notes = Note.all
+  @only_display = false
+  @note = @all_notes[params[:index].to_i]
+  erb :index
+end
+
+put '/notes/:index/edit' do
+  @all_notes = Note.all
+  @note = @all_notes[params[:index].to_i]
+
+  @note.update(content: params[:content])
+  redirect '/'
+end
+
+delete '/notes/:index/edit' do
+  @all_notes = Note.all
+  @note = @all_notes[params[:index].to_i]
+
+  @note.destroy
+  redirect '/'
+end
+
+# get '/info' do
+#   Demo.new(self).info
+# end
+
--- app/helpers/formatting.rb
+++ app/helpers/formatting.rb
@@ -2,4 +2,8 @@ helpers do
   def em(text)
     "<em>#{text}</em>"
   end
+
+  # def current_note
+  #   @current_note ||= Note.where(id: params[:index]).first
+  # end
 end
